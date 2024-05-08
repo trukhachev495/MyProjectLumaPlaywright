08de320f16d462242ae5670109cd2fe044e6f69d
trukhachev495 <sergey.trainings@gmail.com>
2024-05-08 13:20:55 +0400

    RF-POM |menTops.spec.js | Check the name of 14 shopping styles in the Men's/Tops section..1

--- tests/menTopsPage.spec.js
+++ tests/menTopsPage.spec.js
@@ -8,4 +8,16 @@ test.describe('menTops', () => {
 
         await homePage.open();
     })
+    test("Check the name of 14 shopping styles in the Men's/Tops section.", async ({ page }) => {
+        const homePage = new HomePage(page)
+        const menTopsPage = new MenTopsPage(page)
+
+        await homePage.hoverMenLink()
+        await homePage.clickMenTopsLink()
+        await menTopsPage.clickMenTopsStyle()
+        for (let index = 0; index < LIST_STYLE_MEN_TOPS.length; index++) {
+            await expect(menTopsPage.locators.getMenTopsListStyle().nth(index)).toContainText(LIST_STYLE_MEN_TOPS[index])
+        }
+        await expect(page).toHaveURL(BASE_URL + MEN_TOPS_PAGE_END_POINT)
+    });
 })
