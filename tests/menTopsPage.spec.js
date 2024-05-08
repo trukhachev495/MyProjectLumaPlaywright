edc2068b019753c7ec4a4bfa89964add898fa059
trukhachev495 <sergey.trainings@gmail.com>
2024-05-08 13:32:32 +0400

    update test

--- tests/menTopsPage.spec.js
+++ tests/menTopsPage.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
-import HomePage from "..//page_objects/homePage.js"
-import { LIST_STYLE_MEN_TOPS, BASE_URL, MEN_TOPS_PAGE_END_POINT} from "..//helpers/testData.js"
+import HomePage from "..//page_objects/homePage.js";
+import { LIST_STYLE_MEN_TOPS, BASE_URL, MEN_TOPS_PAGE_END_POINT} from "..//helpers/testData.js";
 
 test.describe('menTops', () => {
     test.beforeEach(async ({ page }) => {
@@ -9,15 +9,14 @@ test.describe('menTops', () => {
         await homePage.open();
     })
     test("Check the name of 14 shopping styles in the Men's/Tops section.", async ({ page }) => {
-        const homePage = new HomePage(page)
-        const menTopsPage = new MenTopsPage(page)
+        const homePage = new HomePage(page);
 
-        await homePage.hoverMenLink()
-        await homePage.clickMenTopsLink()
-        await menTopsPage.clickMenTopsStyle()
+        await homePage.hoverMenLink();
+        const menTopsPage = await homePage.clickMenTopsLink();
+        await menTopsPage.clickMenTopsStyle();
         for (let index = 0; index < LIST_STYLE_MEN_TOPS.length; index++) {
             await expect(menTopsPage.locators.getMenTopsListStyle().nth(index)).toContainText(LIST_STYLE_MEN_TOPS[index])
-        }
-        await expect(page).toHaveURL(BASE_URL + MEN_TOPS_PAGE_END_POINT)
+        };
+        await expect(page).toHaveURL(BASE_URL + MEN_TOPS_PAGE_END_POINT);
     });
 })
